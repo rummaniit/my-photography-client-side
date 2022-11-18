ec05caa9606dcf57c999b747f77335eff529f487
rummaniit <rummanoffice12@gmail.com>
2022-11-19 01:07:23 +0600

    done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { AuthServices } from '../../../context/AuthContext/AuthContext';
 import logo from '../../../images/logo.svg'
 
 const Header = () => {
     const { Logout, currentUser } = useContext(AuthServices)
+    // const [clicked, setClicked] = useState(false)
     let activeStyle = {
         backgroundColor: "#301934",
         padding: '15px',
@@ -24,13 +25,14 @@ const Header = () => {
     }
     return (
         <div>
-            <header className="p-6 dark:bg-gray-800 dark:text-gray-100">
-                <div className="container flex justify-between h-16 mx-auto">
+            <header className="p-6 dark:bg-gray-800 dark:text-gray-100 mb-36">
+                <div className=" lg:container flex justify-between h-16 mx-auto relative">
                     <Link to='/' rel="noopener noreferrer" aria-label="Back to homepage" className="flex items-center p-2" >
                         <h1 className='text-purple-300 text-3xl font-bold'>My Photography</h1>
                         <img src={logo} style={logoSize} alt="" />
                     </Link>
-                    <ul className="items-stretch hidden space-x-3 lg:flex">
+
+                    <ul className="sm:flex flex-col justify-center items-center md:flex-row">
                         <li className="flex">
                             <NavLink rel="noopener noreferrer" to='/' className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 "
                                 style={({ isActive }) =>
@@ -38,14 +40,14 @@ const Header = () => {
                                 }>Home</NavLink>
                         </li>
                         <li className="flex">
-                            <NavLink rel="noopener noreferrer" to='/services' className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 "
+                            <NavLink rel="noopener noreferrer" to='/services' className=" flex  items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 "
                                 style={({ isActive }) =>
                                     isActive ? activeStyle : undefined
                                 }
                             >Services</NavLink>
                         </li>
                         <li className="flex">
-                            <NavLink rel="noopener noreferrer" to='/blogs' className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400"
+                            <NavLink rel="noopener noreferrer" to='/blogs' className=" flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400"
                                 style={({ isActive }) =>
                                     isActive ? activeStyle : undefined
                                 }
@@ -58,32 +60,70 @@ const Header = () => {
                                 }
                             >Contact</NavLink>
                         </li> */}
-                    </ul>
-                    <div className="items-center flex-shrink-0 hidden lg:flex">
                         {
-                            currentUser ? <div>
-                                <Link>
+                            currentUser ? <div className='flex flex-col md:flex-row'>
+                                <li className="flex">
+                                    <NavLink rel="noopener noreferrer" onClick={handleLogout} className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 "
+                                        style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                        }
+                                    >Log Out</NavLink>
+                                </li>
+                                {/* <Link>
                                     <button className="self-center px-8 py-3 rounded" onClick={handleLogout}>Log Out</button>
-                                </Link>
-                                <Link to='/addservices'>
+                                </Link> */}
+                                <li className="flex">
+                                    <NavLink rel="noopener noreferrer" to='/addservices' className="flex  items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400"
+                                        style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                        }
+                                    >Add Services</NavLink>
+                                </li>
+
+                                {/* <Link to='/addservices'>
                                     <button className="self-center px-8 py-3 font-semibold rounded ">Add Service</button>
-                                </Link>
-                                <Link to='/myreview'>
+                                </Link> */}
+                                <li className="flex">
+                                    <NavLink rel="noopener noreferrer" to='/myreview' className=" flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400"
+                                        style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                        }
+                                    >My Reviews</NavLink>
+                                </li>
+                                {/* <Link to='/myreview'>
                                     <button className="self-center px-8 py-3 font-semibold rounded ">My Review</button>
-                                </Link>
-                            </div> : <div>
-                                <Link to='/login'>
+                                </Link> */}
+                            </div> : <div className='flex flex-col md:flex-row'>
+                                <li className="flex">
+                                    <NavLink rel="noopener noreferrer" to='/login' className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400"
+                                        style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                        }
+                                    >Login</NavLink>
+                                </li>
+                                <li className="flex sm:bg-purple-900">
+                                    <NavLink rel="noopener noreferrer" to='/register' className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 "
+                                        style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                        }
+                                    >Register</NavLink>
+                                </li>
+                                {/* <Link to='/login'>
                                     <button className="self-center px-8 py-3 rounded">Sign in</button>
-                                </Link>
-                                <Link to='/register'>
+                                </Link> */}
+                                {/* <Link to='/register'>
                                     <button className="self-center px-8 py-3 font-semibold rounded ">Sign up</button>
-                                </Link>
+                                </Link> */}
 
                             </div>
                         }
 
+                    </ul>
+                    <div className="items-center flex-shrink-0 hidden lg:flex">
+
+
                     </div>
-                    <button className="p-4 lg:hidden">
+                    <button className="p-4 lg:hidden md:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
